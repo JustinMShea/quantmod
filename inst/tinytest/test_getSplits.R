f6e0e0f780b5cb557e7cfcdba90bfbced725f9bf
JustinMShea <jshea@roosevelt.edu>
2021-09-25 13:56:32 -0500

    #317 First function refactor seems to work, legacy RUnit code commented out for now.

--- inst/tinytest/test_getSplits.R
+++ inst/tinytest/test_getSplits.R
@@ -1,10 +1,14 @@
 library(quantmod)
 
-test.web.endpoints <- Sys.getenv("QUANTMOD_TEST_WEB_ENDPOINTS")
-
+# test.web.endpoints <- Sys.getenv("QUANTMOD_TEST_WEB_ENDPOINTS")
 # Ensure getSplits() returns the expected ratio
-if (nzchar(test.web.endpoints)) {
-  aapl.spl <- as.numeric(getSplits("AAPL")["/2018"])
-  expected <- c(0.5, 0.5, 0.5, 1/7)
-  stopifnot(isTRUE(all.equal(aapl.spl, expected)))
-}
+# if (nzchar(test.web.endpoints)) {
+#  aapl.spl <- as.numeric(getSplits("AAPL")["/2018"])
+#  expected <- c(0.5, 0.5, 0.5, 1/7)
+#  stopifnot(isTRUE(all.equal(aapl.spl, expected)))
+# }
+
+aapl.spl <- as.numeric(quantmod::getSplits("AAPL")["/2018"])
+expected <- c(0.5, 0.5, 0.5, 1/7)
+
+tinytest::expect_equal(aapl.spl, expected)
